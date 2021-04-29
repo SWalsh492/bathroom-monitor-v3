c88da5fa087e1bd77a1002b4d4a086dd2de75e2a
SWalsh492 <83427120+SWalsh492@users.noreply.github.com>
2021-04-29 20:32:35 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+function _Distance () {
+    pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    Distance = pins.pulseIn(DigitalPin.P2, PulseValue.High)
+    Distance = Distance / 58
+}
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(Distance)
+})
+let Entry_True_or_False = false
+let Total_Entry = 0
+let Distance = 0
+Distance = 0
+let Entry = 0
+radio.setGroup(111)
+basic.showNumber(Entry)
+basic.forever(function () {
+    _Distance()
+    if (Distance <= 40) {
+        if (Distance <= 30) {
+            Entry += 1
+            basic.showNumber(Entry)
+            Total_Entry += 1
+            radio.sendNumber(Entry)
+            Entry_True_or_False = true
+            basic.pause(2000)
+        }
+    }
+})
+basic.forever(function () {
+    if (Entry == 2) {
+        basic.pause(40000)
+        while (Entry >= 2) {
+            music.playMelody("C C C C C C C C ", 60)
+        }
+    } else if (Entry == 3) {
+        while (Entry == 3) {
+            music.playMelody("C C C C C C C C ", 60)
+        }
+    }
+})
+basic.forever(function () {
+    if (input.rotation(Rotation.Roll) >= 120 && Entry_True_or_False == true) {
+        Entry += -1
+        basic.showNumber(Entry)
+        Entry_True_or_False = false
+    } else if (input.rotation(Rotation.Roll) >= 120 && Entry == 2) {
+        Entry += -1
+        basic.showNumber(Entry)
+    }
+})
